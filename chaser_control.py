d5d7708a42ee08c63761780cd0e006452c9a0ee9
EmptyJackson <matthew@hmjackson.com>
2020-03-05 17:17:50 +0000

    Add template for chaser control

--- chaser_control.py
+++ chaser_control.py
@@ -1,33 +1,5 @@
 import rospy
 
-class GroundtruthPose(object):
-  def __init__(self, name='turtlebot3_burger'):
-    rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
-    self._pose = np.array([np.nan, np.nan, np.nan], dtype=np.float32)
-    self._name = name
-
-  def callback(self, msg):
-    idx = [i for i, n in enumerate(msg.name) if n == self._name]
-    if not idx:
-      raise ValueError('Specified name "{}" does not exist.'.format(self._name))
-    idx = idx[0]
-    self._pose[X] = msg.pose[idx].position.x
-    self._pose[Y] = msg.pose[idx].position.y
-    _, _, yaw = euler_from_quaternion([
-        msg.pose[idx].orientation.x,
-        msg.pose[idx].orientation.y,
-        msg.pose[idx].orientation.z,
-        msg.pose[idx].orientation.w])
-    self._pose[YAW] = yaw
-
-  @property
-  def ready(self):
-    return not np.isnan(self._pose[0])
-
-  @property
-  def pose(self):
-    return self._pose
-
 def run(args):
   rospy.init_node('obstacle_avoidance')
   avoidance_method = globals()[args.mode]
@@ -64,8 +36,8 @@ def run(args):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Runs single chaser')
-  parser.add_argument('--name', action='store', default='c1', help='Chaser name.', choices=['c1', 'c2', 'c3'])
+  parser = argparse.ArgumentParser(description='Runs centralised chaser control')
+  parser.add_argument('--mode', action='store', default='simple', help='Method.', choices=['simple'])
   args, unknown = parser.parse_known_args()
   try:
     run(args)
