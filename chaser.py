f8e7b6c794477a9d141c1cac303f738d4127a671
EmptyJackson <matthew@hmjackson.com>
2020-03-05 23:13:49 +0000

    Update chaser framework

--- chaser.py
+++ chaser.py
@@ -1,71 +1,67 @@
+#!/usr/bin/env python
+import argparse
 import rospy
+import numpy as np
 
-class GroundtruthPose(object):
-  def __init__(self, name='turtlebot3_burger'):
-    rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
-    self._pose = np.array([np.nan, np.nan, np.nan], dtype=np.float32)
-    self._name = name
+# Robot motion commands:
+# http://docs.ros.org/api/geometry_msgs/html/msg/Twist.html
+from geometry_msgs.msg import Twist, PoseArray
+# For groundtruth information.
+from gazebo_msgs.msg import ModelStates
+from tf.transformations import euler_from_quaternion
+
+from common import MultiGroundtruthPose
+
+class PathSubscriber(object):
+  def __init__(self, name='c0'):
+    rospy.Subscriber('/'+name+'/path', PoseArray, self.callback)
+    self._path = []
 
   def callback(self, msg):
-    idx = [i for i, n in enumerate(msg.name) if n == self._name]
-    if not idx:
-      raise ValueError('Specified name "{}" does not exist.'.format(self._name))
-    idx = idx[0]
-    self._pose[X] = msg.pose[idx].position.x
-    self._pose[Y] = msg.pose[idx].position.y
-    _, _, yaw = euler_from_quaternion([
-        msg.pose[idx].orientation.x,
-        msg.pose[idx].orientation.y,
-        msg.pose[idx].orientation.z,
-        msg.pose[idx].orientation.w])
-    self._pose[YAW] = yaw
+    self._path = [pose.position for pose in msg.poses]
 
   @property
   def ready(self):
-    return not np.isnan(self._pose[0])
+    return not np.isnan(self._path[0])
 
   @property
-  def pose(self):
-    return self._pose
+  def path(self):
+    return self._path
+
+
+def path_follow(path, position):
+
 
 def run(args):
-  rospy.init_node('obstacle_avoidance')
-  avoidance_method = globals()[args.mode]
+  c_id = args.id
+  c_name = 'c' + str(c_id)
+  rospy.init_node('chaser' + str(c_id))
 
-  # Update control every 100 ms.
+  # Update control every 10 ms.
   rate_limiter = rospy.Rate(100)
-  publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
-  laser = SimpleLaser()
-  # Keep track of groundtruth position for plotting purposes.
-  groundtruth = GroundtruthPose()
-  pose_history = []
-  with open('/tmp/gazebo_exercise.txt', 'w'):
-    pass
+  publisher = rospy.Publisher('/' + c_name + '/cmd_vel', Twist, queue_size=5)
+
+  groundtruth = MultiGroundtruthPose([c_name])
+  path = PathSubscriber(c_name)
 
   while not rospy.is_shutdown():
     # Make sure all measurements are ready.
-    if not laser.ready or not groundtruth.ready:
+    if not groundtruth.ready or not path.ready:
       rate_limiter.sleep()
       continue
 
-    u, w = avoidance_method(*laser.measurements)
+    # Calculate and publish control inputs.
+    u, w = path_follow(path, groundtruth.poses[])
     vel_msg = Twist()
     vel_msg.linear.x = u
     vel_msg.angular.z = w
     publisher.publish(vel_msg)
-
-    # Log groundtruth positions in /tmp/gazebo_exercise.txt
-    pose_history.append(groundtruth.pose)
-    if len(pose_history) % 10:
-      with open('/tmp/gazebo_exercise.txt', 'a') as fp:
-        fp.write('\n'.join(','.join(str(v) for v in p) for p in pose_history) + '\n')
-        pose_history = []
     rate_limiter.sleep()
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Runs single chaser')
-  parser.add_argument('--name', action='store', default='c1', help='Chaser name.', choices=['c1', 'c2', 'c3'])
+  parser.add_argument('--id', action='store', default=0, type=int, help='Chaser id.', choices=[0, 1, 2])
   args, unknown = parser.parse_known_args()
   try:
     run(args)
--- chaser_control.py
+++ chaser_control.py
@@ -1,43 +1,55 @@
+#!/usr/bin/env python
 import rospy
+from common import MultiGroundtruthPose
+
+from geometry_msgs.msg import PoseArray, Pose, Point
+
+def simple(poses):
+  # Return paths as array of Point
+  pass
+
+def create_pose_array(path):
+  pose_path = []
+  for position in path:
+    p = Pose()
+    p.position = position
+    pose_path.append(p)
+  path_msg = PoseArray()
+  path_msg.poses = pose_path
+  return path_msg
 
 def run(args):
-  rospy.init_node('obstacle_avoidance')
-  avoidance_method = globals()[args.mode]
+  rospy.init_node('chaser_control')
+  nav_method = globals()[args.mode]
 
-  # Update control every 100 ms.
-  rate_limiter = rospy.Rate(100)
-  publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
-  laser = SimpleLaser()
-  # Keep track of groundtruth position for plotting purposes.
-  groundtruth = GroundtruthPose()
-  pose_history = []
-  with open('/tmp/gazebo_exercise.txt', 'w'):
-    pass
+  # Update paths every 100 ms.
+  rate_limiter = rospy.Rate(10)
+  publishers = 
+    [rospy.Publisher('/c'+str(i)+'/path', PoseArray, queue_size=1)]
+
+  gts = MultiGroundtruthPose(['c0', 'c1', 'c2', 'r0', 'r1', 'r2'])
 
   while not rospy.is_shutdown():
-    # Make sure all measurements are ready.
-    if not laser.ready or not groundtruth.ready:
+    # Make sure all groundtruths are ready.
+    if not gts.ready:
       rate_limiter.sleep()
       continue
 
-    u, w = avoidance_method(*laser.measurements)
-    vel_msg = Twist()
-    vel_msg.linear.x = u
-    vel_msg.angular.z = w
-    publisher.publish(vel_msg)
+    print(gts.poses)
 
-    # Log groundtruth positions in /tmp/gazebo_exercise.txt
-    pose_history.append(groundtruth.pose)
-    if len(pose_history) % 10:
-      with open('/tmp/gazebo_exercise.txt', 'a') as fp:
-        fp.write('\n'.join(','.join(str(v) for v in p) for p in pose_history) + '\n')
-        pose_history = []
+    # todo: Calculate paths
+    paths = nav_method(gts.poses)
+
+    for path, publisher in zip(paths, publishers):
+      path_msg = create_pose_array(path)
+      publisher.publish(path_msg)
+
     rate_limiter.sleep()
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Runs centralised chaser control')
-  parser.add_argument('--mode', action='store', default='simple', help='Method.', choices=['simple'])
+  parser.add_argument('--mode', action='store', default='simple', help='Routing method.', choices=['simple'])
   args, unknown = parser.parse_known_args()
   try:
     run(args)
