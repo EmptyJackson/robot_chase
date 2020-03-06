00572cae33e586481fb744faa63ae42b920e9fd1
EmptyJackson <matthew@hmjackson.com>
2020-03-06 13:47:51 +0000

    Add occupancy grid

--- common.py
+++ common.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 
 import argparse
 import numpy as np
+import scipy
 import rospy
 import random
 import math
@@ -23,6 +24,22 @@ from tf.transformations import euler_from_quaternion
 X = 0
 Y = 1
 YAW = 2
+
+ROBOT_RADIUS = 0.105 / 2.
+CYLINDER_POSITIONS = np.array(
+  [[-6., 6.], 
+   [-2., 2.],
+   [ 2.,-6.],
+   [ 6.,-2.]], dtype=np.float32)
+CYLINDER_RADIUS = 1. + ROBOT_RADIUS
+WALL_WIDTH = ROBOT_RADIUS + 0.15 / 2.
+DOOR_BOUNDS = np.array([1.5, 2.5], dtype=np.float32)
+GRID_FREQ = 4.
+ARENA_OFFSET = 8. - ROBOT_RADIUS
+RESOLUTION = 0.05
+
+FREE = 0
+OCCUPIED = 2
 
 class MultiGroundtruthPose(object):
   def __init__(self, names):
@@ -94,3 +111,85 @@ def get_velocity(position, path_points):
 
   return v
 
+# Defines an occupancy grid.
+class OccupancyGrid(object):
+  def __init__(self, values, origin, resolution):
+    self._original_values = values.copy()
+    self._values = values.copy()
+    # Inflate obstacles (using a convolution).
+    inflated_grid = np.zeros_like(values)
+    inflated_grid[values == OCCUPIED] = 1.
+    w = 2 * int(ROBOT_RADIUS / resolution) + 1
+    inflated_grid = scipy.signal.convolve2d(inflated_grid, np.ones((w, w)), mode='same')
+    self._values[inflated_grid > 0.] = OCCUPIED
+    self._origin = np.array(origin[:2], dtype=np.float32)
+    self._origin -= resolution / 2.
+    assert origin[YAW] == 0.
+    self._resolution = resolution
+
+  @property
+  def values(self):
+    return self._values
+
+  @property
+  def resolution(self):
+    return self._resolution
+
+  @property
+  def origin(self):
+    return self._origin
+
+  def draw(self):
+    plt.imshow(self._original_values.T, interpolation='none', origin='lower',
+               extent=[self._origin[X],
+                       self._origin[X] + self._values.shape[0] * self._resolution,
+                       self._origin[Y],
+                       self._origin[Y] + self._values.shape[1] * self._resolution])
+    plt.set_cmap('gray_r')
+
+  def get_index(self, position):
+    idx = ((position - self._origin) / self._resolution).astype(np.int32)
+    if len(idx.shape) == 2:
+      idx[:, 0] = np.clip(idx[:, 0], 0, self._values.shape[0] - 1)
+      idx[:, 1] = np.clip(idx[:, 1], 0, self._values.shape[1] - 1)
+      return (idx[:, 0], idx[:, 1])
+    idx[0] = np.clip(idx[0], 0, self._values.shape[0] - 1)
+    idx[1] = np.clip(idx[1], 0, self._values.shape[1] - 1)
+    return tuple(idx)
+
+  def get_position(self, i, j):
+    return np.array([i, j], dtype=np.float32) * self._resolution + self._origin
+
+  def is_occupied(self, position):
+    return self._values[self.get_index(position)] == OCCUPIED
+
+  def is_free(self, position):
+    return self._values[self.get_index(position)] == FREE
+
+
+def get_occupancy_grid():
+  dim = int(2 * ARENA_OFFSET / RESOLUTION)
+  grid = np.zeros((dim, dim), dtype=np.int8)
+
+  for x in range(dim):
+    for y in range(dim):
+      position = np.array([x, y])
+      position = (position - (dim/2)) * 2
+      if collision(position):
+        grid[x, y] = OCCUPIED
+  return OccupancyGrid(grid, [dim/2, dim/2], RESOLUTION)
+
+def collision(position):
+  # Arena
+  if np.any(np.abs(position) > ARENA_OFFSET):
+    return True
+
+  # Cylinders
+  for c in CYLINDER_POSITIONS:
+    if np.linalg.norm(c - position) > CYLINDER_RADIUS:
+      return True
+
+  rel_pos = position % GRID_FREQ
+  return np.all(np.logical_or(
+    position < WALL_WIDTH,
+    position > GRID_FREQ - WALL_WIDTH))
