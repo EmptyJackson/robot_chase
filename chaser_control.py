5a33f70ec62afcd784089987a1d02f14dcab76fe
EmptyJackson <matthew@hmjackson.com>
2020-03-07 14:08:51 +0000

    Add Particle cloud

--- chaser_control.py
+++ chaser_control.py
@@ -1,9 +1,84 @@
 #!/usr/bin/env python
 import rospy
+import random
 import argparse
-from common import MultiGroundtruthPose
+import numpy as np
+from common import *
 
 from geometry_msgs.msg import PoseArray, Pose, Point
+
+OCC_GRID = get_occupancy_grid()
+
+def in_line_of_sight(p1, p2):
+  sample_rate = 10 # Samples per meter
+  n_samples = np.linalg.norm(p1 - p2) * sample_rate
+  x = np.linspace(p1[X], p2[X], n_samples)
+  y = np.linspace(p1[Y], p2[Y], n_samples)
+  for coord in zip(x, y):
+    if OCC_GRID.is_occupied(coord):
+      return False
+  return True
+
+class Particle:
+  def __init__(self, max_speed, chasers, position=None):
+    self._max_speed = max_speed
+    if not position is None:
+      self._position = position
+    else:
+      self._position = np.random.uniform(-ARENA_OFFSET, ARENA_OFFSET, 2)
+      while not self.is_valid(chasers):
+        self._position = np.random.uniform(-ARENA_OFFSET, ARENA_OFFSET, 2)
+
+  def is_valid(self, chasers):
+    if not OCC_GRID.is_free(self._position):
+      return False
+    for c in chasers:
+      if in_line_of_sight(self._position, c):
+        return False
+    return True
+
+  def step(self):
+    d = np.random.uniform(-1., 1., 2)
+    speed = np.random.uniform(0., self._max_speed)
+    return speed * d / np.linalg.norm(d)
+
+  @property
+  def position(self):
+    return self._position
+  
+
+class ParticleCloud:
+  def __init__(self, num_points, max_speed, chasers, start_pos=None):
+    self._num_points = num_points
+    self._max_speed = max_speed
+    if start_pos is None:
+      self.reset(chasers)
+    else:
+      self._particles = set()
+      for _ in range(self._num_points):
+        self._particles.append(Particle(self._max_speed, chasers, start_pos))
+
+  def reset(self, chasers):
+    self._particles = set()
+    for _ in range(self._num_points):
+      self._particles.append(Particle(self._max_speed, chasers))
+
+  def update(self, chasers):
+    invalid_ps = set()
+    for particle in self._particles:
+      invalid = particle.step(chasers)
+      if invalid:
+        invalid_ps.add(particle)
+
+    n_invalid = len(invalid_ps)
+    if n_invalid == self._num_points:
+      # Reset cloud when all particles invalidated
+      self.reset()
+    else:
+      self._particles.remove(invalid_ps)
+      for _ in range(n_invalid):
+        pos = random.sample(self._particles, 1)[0].position
+        self._particles.add(particle(self._max_speed, chasers, pos))
 
 def simple(poses):
   # Return paths as array of Point
