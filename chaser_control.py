3fad3ff517e9b4a9d5298622836c26762d0f4d92
EmptyJackson <matthew@hmjackson.com>
2020-03-09 16:17:12 +0000

    Truncate path for chaser potential field

--- chaser_control.py
+++ chaser_control.py
@@ -134,6 +134,7 @@ def simple(poses, allocations, runner_ests):
   return paths
 
 def rrt(poses, allocations, runner_ests):
+  path_tail_max = 10
   occupancy_grid = get_occupancy_grid()
   potential_field = PotentialField({}, is_path=True)
   paths = {}
@@ -161,8 +162,9 @@ def rrt(poses, allocations, runner_ests):
     draw_solution(s, g)
     plt.show()
       
+    path_tail = path[-min(len(path), path_tail_max):]
+    potential_field.add_target(c, path_tail)
     paths[c] = []
-    potential_field.add_target(c, path)
     for point in path:
       paths[c].append(position_to_point(point))
   return paths
