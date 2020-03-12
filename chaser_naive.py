5f346b76e9dfe1809329a263c0839431c37b426d
Ryxuma <tryfan99@gmail.com>
2020-03-12 13:22:28 +0000

    Simple chaser with broken lasers

--- chaser_naive.py
+++ chaser_naive.py
@@ -16,36 +16,129 @@ from common import *
 OCC_GRID = get_occupancy_grid()
 RUNNERS = ['r0', 'r1', 'r2']
 
-
-class PathSubscriber(object):
-  def __init__(self, name='c0'):
-    rospy.Subscriber('/'+name+'/path', PoseArray, self.callback)
-    self._path = None
+class SimpleLaser(object):
+  def __init__(self):
+    rospy.Subscriber('/c0/scan', LaserScan, self.callback)
+    self._angles = [0., np.pi / 4., -np.pi / 4., np.pi / 2., -np.pi / 2.]
+    self._width = np.pi / 180. * 10.  # 10 degrees cone of view.
+    self._measurements = [float('inf')] * len(self._angles)
+    self._indices = None
 
   def callback(self, msg):
-    self._path = np.array([[pose.position.x, pose.position.y] for pose in msg.poses])
+    # Helper for angles.
+    def _within(x, a, b):
+      pi2 = np.pi * 2.
+      x %= pi2
+      a %= pi2
+      b %= pi2
+      if a < b:
+        return a <= x and x <= b
+      return a <= x or x <= b;
+
+    # Compute indices the first time.
+    if self._indices is None:
+      self._indices = [[] for _ in range(len(self._angles))]
+      for i, d in enumerate(msg.ranges):
+        angle = msg.angle_min + i * msg.angle_increment
+        for j, center_angle in enumerate(self._angles):
+          if _within(angle, center_angle - self._width / 2., center_angle + self._width / 2.):
+            self._indices[j].append(i)
+
+    ranges = np.array(msg.ranges)
+    for i, idx in enumerate(self._indices):
+      # We do not take the minimum range of the cone but the 10-th percentile for robustness.
+      self._measurements[i] = np.percentile(ranges[idx], 10)
 
   @property
   def ready(self):
-    return not self._path is None
+    return not np.isnan(self._measurements[0])
 
   @property
-  def path(self):
-    return self._path
+  def measurements(self):
+    return self._measurements
 
-def in_line_of_sight(p1, p2):
-  sample_rate = 10 # Samples per meter
-  n_samples = np.linalg.norm(p1 - p2) * sample_rate
-  x = np.linspace(p1[X], p2[X], n_samples)
-  y = np.linspace(p1[Y], p2[Y], n_samples)
-  for coord in zip(x, y):
-    if OCC_GRID.is_occupied(coord):
-      return False
-  return True
 
+def braitenberg(front, front_left, front_right, left, right):
+  s = np.tanh([left, front_left, front, front_right, right])
+  s = [1 - x for x in s]
+
+  u_weights = [0, -0.5, -1, -0.5, 0]
+  w_weights = [-0.5, -1, 0, 1, 0.5]
+
+  u = np.dot(s, u_weights) + 1.
+  w = np.dot(s, w_weights)
+
+  return u, w
+
+def rule_based(front, front_left, front_right, left, right):
+  u = 0.
+  w = 0.
+
+  SLOW_SPEED = 0.1
+  FAST_SPEED = 0.3
+  TURN_SPEED = 0.6
+
+  d = 0.5
+  vcd = 0.15
+
+  f = front < d
+  fl = front_left < d or left < d / 3.
+  fr = front_right < d or right < d / 3.
+
+  very_close = front < vcd or front_left < vcd or front_right < vcd
+
+  #111
+  if f and fl and fr:
+    if very_close:
+      u = -SLOW_SPEED
+      w = 0
+    else:
+      u = SLOW_SPEED
+      w = TURN_SPEED
+  #110
+  elif f and fl and not fr:
+    u = SLOW_SPEED
+    w = -TURN_SPEED
+  #011
+  elif f and not fl and fr:
+    u = SLOW_SPEED
+    w = TURN_SPEED
+  #010
+  elif f and not fl and not fr:
+    if very_close:
+      u = -SLOW_SPEED
+      w = 0
+    else:
+      u = SLOW_SPEED
+      w = TURN_SPEED
+  #101
+  elif not f and fl and fr:
+    if very_close:
+      u = -SLOW_SPEED
+      w = 0
+    else:
+      u = SLOW_SPEED
+      w = TURN_SPEED
+  #100
+  elif not f and fl and not fr:
+    u = SLOW_SPEED
+    w = -TURN_SPEED
+  #001
+  elif not f and not fl and fr:
+    u = SLOW_SPEED
+    w = TURN_SPEED
+  #000
+  elif not f and not fl and not fr:
+    u = FAST_SPEED
+    w = 0
+  else:
+    print('err invalid sensor config:', f, fl, fr)
+    
+  return u, w
 
 def capture_runner(r_name_msg):
   RUNNERS.remove(r_name_msg.data)
+
 
 def run(args):
   c_id = args.id
@@ -61,23 +154,22 @@ def run(args):
   rospy.Subscriber('/captured_runners', String, capture_runner)
   
   groundtruth = MultiGroundtruthPose([c_name] + RUNNERS)
-  path_sub = PathSubscriber(c_name)
-
-  rev_frames = 0
-  had_path = False
+  laser = SimpleLaser()
 
   frame_id = 0
   init_publish = False
+
+  print('starting')
+  
   while not rospy.is_shutdown():
     # Make sure all measurements are ready.
-    if not groundtruth.ready:
+    if not laser.ready or not groundtruth.ready:
       rate_limiter.sleep()
       continue
-
     pose = groundtruth.poses[c_name]
 
     # Publish initial position
-    if not init_publish or not path_sub.ready:
+    if not init_publish:
       if RVIZ_PUBLISH:
         position_msg = PointStamped()
         position_msg.header.seq = frame_id
@@ -92,24 +184,11 @@ def run(args):
       init_publish = True
       rate_limiter.sleep()
       continue
-
-    for runner in ['r0', 'r1', 'r2']:
-      if np.linalg.norm(groundtruth.poses[runner][:2] - pose[:2]) < 0.15:
-        rev_frames = 50
-
-    path = path_sub.path
-
-    if path is None or len(path) == 0:
-      if had_path:
-        rev_frames = 50
-
-    else:
-      had_path = True
     
-    
-    # Calculate and publish control inputs.
-    v = get_velocity(pose[:2], path, CHASER_SPEED)
+    v = braitenberg(*laser.measurements)
+    print(laser.measurements)
 
+    # Calculate movement
     for runner in RUNNERS:
       runner_pose = groundtruth.poses[runner]
       runner_pos = runner_pose[:2]
@@ -117,7 +196,7 @@ def run(args):
       diff = runner_pos - pose[:2]
       dist = np.linalg.norm(diff)
       
-      if dist < 1 and in_line_of_sight(runner_pos, pose[:2]):
+      if dist < 3:
         f_pos = runner_pos + np.array([np.cos(runner_pose[2]), np.sin(runner_pose[2])]) * dist
         f_pos_diff = f_pos - pose[:2]
         f_pos_dist = np.linalg.norm(f_pos_diff)
@@ -125,9 +204,6 @@ def run(args):
     
     u, w = feedback_linearized(pose, v, 0.1)
 
-    if rev_frames > 0:
-      u = -CHASER_SPEED
-      w = 0
     
     vel_msg = Twist()
     vel_msg.linear.x = u
@@ -148,8 +224,6 @@ def run(args):
 
     rate_limiter.sleep()
     frame_id += 1
-    if rev_frames > 0:
-      rev_frames -= 1
 
 
 if __name__ == '__main__':
